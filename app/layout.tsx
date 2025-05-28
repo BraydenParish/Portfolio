412a8828a2828e34b957e8a4562a9a9d1287a586
Brayden Parish <95709729+BraydenParish@users.noreply.github.com>
2025-05-27 23:23:57 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Brayden Parish',
+  description: 'Created by Brayden Parish',
   generator: 'v0.dev',
 }
 
